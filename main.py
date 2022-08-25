7cb735a0369a7cadd01225c04a3a6a0e3f0b646e
SKATHEKING <mateusouro1@hotmail.com>
2022-08-25 11:07:57 +0100

    initial crash course set

---
+++ main.py
@@ -0,0 +1,33 @@
+# The aim of this project is to serve as a python refresher so that the user may reread when revising python code
+
+#set variable
+name = 'Mateus Goncalves De Ouro'
+#int variable
+age = 24
+#boolean variable
+isMale = True
+
+print(f'Hello my name is {name}, I am {age} years old and yes it is {isMale} I am a man.')
+
+#tuples are an immutable data type
+favShoeBrands = ('DC','Nike','Adidas', 'C1rca', 'Supra')
+
+#lists
+
+coolCarsList = ['Nissan skyline', 'Nissan GTR', ' Hellcat', 'Range rover']
+
+#dictionaries
+
+university_grades = {'mateus ' : '2:1', 'sol' : '2:1' , 'chris' : '1' }
+
+def get_university_grades():
+    for key,value in university_grades.items():
+        print(f'the student {key} has achieved {value} in his academic record')
+get_university_grades()
+
+#defining a function and using it to print all coolCarsList
+def allCoolCars():
+    for car in coolCarsList:
+        print(f'{car} is a super cool car!')
+
+allCoolCars()
